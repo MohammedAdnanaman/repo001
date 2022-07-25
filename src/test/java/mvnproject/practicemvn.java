71ff6cc1085b2ae19b7a676ae8c698565dacf605
MohammedAdnanaman <adnanaman1999@gmail.com>
2022-07-25 11:51:17 +0530

    my commit first

---
+++ src/test/java/mvnproject/practicemvn.java
@@ -0,0 +1,50 @@
+package mvnproject;
+
+public class practicemvn {
+	
+	public void test() {
+		
+		String URL =System.getProperty("URL");
+		String Browser =System.getProperty("Browser");
+		String userName =System.getProperty("UN");
+		String Password =System.getProperty("PWD");
+		
+		System.out.println("ADNAN");
+		System.out.println("URL =="+URL);
+		System.out.println("Browser =="+Browser);
+		System.out.println("UN =="+userName);
+		System.out.println("PWD =="+Password);
+		System.out.println("end");
+		
+
+	}
+
+	public void test1() {
+		System.out.println("tyss02");
+		System.out.println("end");
+		
+				
+		
+
+	}
+	
+	public void test2() {
+		System.out.println("tyss03");
+
+	}
+	
+	public void test3() {
+		System.out.println("tyss04");
+		
+	}
+	public void test4() {
+		System.out.println("tyss05");
+
+	}
+	public static void main(String[] args) {
+		practicemvn obj= new practicemvn();
+		obj.test();
+		
+	}
+}
+
